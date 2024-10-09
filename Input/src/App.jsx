9d707a88c8c541cdc0a23e812a0301faf8078276
Carlos-Felipe Rodriguez <feliperod0519@hotmail.com>
2024-10-09 09:49:52 -0400

    Felipe Rod

--- Input/src/App.jsx
+++ Input/src/App.jsx
@@ -1,12 +1,33 @@
-import Player from './components/Player.jsx';
+import {useRef} from "react";
+import ResultInput from './components/Input.jsx';
 
-function App() {
+export const userData = {
+  name: '',
+  email: '',
+};
+
+export function App() {
+
+  const inputRefName = useRef();
+  const inputRefEmail = useRef();
+
+  function handleSaveData() {
+    userData.name = inputRefName.current.value;
+    userData.email = inputRefEmail.current.value;
+
+    console.log(userData);
+  }
+
   return (
-    <>
-      <Player />
-      <div id="challenges"></div>
-    </>
+    <div id="app">
+      <ResultInput ref={inputRefName} type="text" label="Your Name" />
+      <ResultInput ref={inputRefEmail} type="text" label="Your E-Mail" />
+      <p id="actions">
+        <button onClick={handleSaveData}>Save Data</button>
+      </p>
+    </div>
   );
 }
-
 export default App;
+
+
