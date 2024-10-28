ec2d18b688e5726c74418f127bd14d737c9344a4
Carlos-Felipe Rodriguez <feliperod0519@hotmail.com>
2024-10-27 21:38:50 -0400

    yellow

--- Reducer/src/App.jsx
+++ Reducer/src/App.jsx
@@ -1,17 +1,53 @@
+import React from 'react';
+
+
+export function counterReducer(state, action) {
+  
+  switch (action.type) {
+    case "INCREMENT":
+      console.log('state-i',state);
+      return state + 1;
+    case "DECREMENT":
+      console.log('state-d',state);
+      return state - 1;
+    case "RESET":
+      console.log('state-r',state);
+      return 0;
+    default:
+      return state;
+  }
+}
+
 function App() {
+
+  const [counter, dispatch] = React.useReducer(counterReducer, 0);
+
+  function handleClick(action) {
+    console.log(action);
+    switch (action) {
+      case 'i':
+        dispatch({type: 'INCREMENT'});
+        break;
+      case 'd':
+        dispatch({type: 'DECREMENT'});
+        break;
+      case 'r':
+        dispatch({type: 'RESET'});
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
+    <div id="app">
+      <h1>The (Final?) Counter</h1>
+      <p id="actions">
+        <button onClick={()=>handleClick('i')}>Increment</button>
+        <button onClick={()=>handleClick('d')}>Decrement</button>
+        <button onClick={()=>handleClick('r')}>Reset</button>
+      </p>
+      <p id="counter">{counter}</p>
     </div>
   );
 }
